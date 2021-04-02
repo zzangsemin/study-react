9d56efe6046b110f50c95e5070ae4a70f1146418
조세민 <1015tpals@gmail.com>
2021-04-03 00:59:29 +0900

    upload mashup-todolist project - 튜토리얼 따라하기

---
+++ mashup-todolist/src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { createGlobalStyle } from "styled-components";
+import TodoCreate from "./components/TodoCreate";
+import TodoHead from "./components/TodoHead";
+import TodoList from "./components/TodoList";
+import TodoTemplate from "./components/TodoTemplate";
+import { TodoProvider } from "./TodoContext";
+
+const GlobalStyle = createGlobalStyle`
+  body{
+    background: #e9ecef;
+  }
+`;
+
+function App() {
+  return (
+    <TodoProvider>
+      <GlobalStyle />
+      <TodoTemplate>
+        <TodoHead />
+        <TodoList />
+        <TodoCreate />
+      </TodoTemplate>
+    </TodoProvider>
+  );
+}
+
+export default App;
